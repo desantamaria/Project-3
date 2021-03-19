fd96007558eadf2161cc13c09c5e7ffb2179b061
Daniel Santamaria <santamaria.daniel316@gmail.com>
2021-03-18 18:27:47 -0700

    Added javadoc comments

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,14 @@
+/**
+@author Daniel Santamaria
+@author Renwell Queyquep
+CS2400
+3/18/21 
+ */
+
+/**
+ * A class of trees whose entries are stored in binary nodes 
+ * in which each node has at most two children. 
+ */
 public class BinaryTree<T> implements BinaryTreeInterface<T> {
     private BinaryNode<T> root;
     
@@ -13,16 +24,34 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
         initializeTree(rootData, leftTree, rightTree);
     } // end constructor
     
+    /**
+     * Creates new tree out of pre-existing trees.
+     *
+     * @param rootData The data that is to be set as the root node.
+     * @param leftTree A pre-existing tree that is to be set as the Left tree. 
+     * @param rightTree A pre-existing tree that is to be set as the Right tree.
+     */
     public void setTree(T rootData, BinaryTreeInterface<T> leftTree,
                         BinaryTreeInterface<T> rightTree) {
         initializeTree(rootData, (BinaryTree<T>) leftTree,
                 (BinaryTree<T>) rightTree);
     } // end setTree
     
+    /**
+     * Sets the desired data as the root node.
+     *
+     * @param rootData The data that is to be set as the root node.
+     */
     public void setRootData(T rootData) {
         root.setData(rootData);
     } // end setRootData
     
+    /**
+     * Returns the root node data.
+     *
+     * @throws EmptyTreeException 
+     * @return the data attached to the root node.
+     */
     public T getRootData() {
         if (isEmpty())
             throw new EmptyTreeException();
@@ -30,22 +59,47 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
             return root.getData();
     } // end getRootData
     
+    /**
+     * Checks if the tree is empty by checking if the root node is null.
+     *
+     * @return whether the root is null or not (T/F).
+     */
     public boolean isEmpty() {
         return root == null;
     } // end isEmpty
     
+    /**
+     * Sets the root node to null.
+     */
     public void clear() {
         root = null;
     } // end clear
     
+    /**
+     * Sets the desired node as the root node.
+     *
+     * @param rootNode The node that is to be set as the root node.
+     */
     protected void setRootNode(BinaryNode<T> rootNode) {
         root = rootNode;
     } // end setRootNode
     
+    /**
+     * Returns the root node.
+     *
+     * @return the root node.
+     */
     protected BinaryNode<T> getRootNode() {
         return root;
     } // end getRootNode
     
+    /**
+     * Initializes the trees and creates the node of the new tree.
+     *
+     * @param rootData The data that is to be set as the root node.
+     * @param leftTree A tree that is to be set as the Left tree. 
+     * @param rightTree A tree that is to be set as the Right tree.
+     */
     private void initializeTree(T rootData, BinaryTree<T> leftTree, BinaryTree<T> rightTree) {
         root = new BinaryNode<>(rootData);
         
@@ -66,35 +120,25 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
             rightTree.clear();
     } // end initializeTree
     
-    /* -------------------------------------------------------------------- */
-    /* Task 1: Implement the 4 methods
-     *     . In BinaryTree.java
-     *          1. public void postorderTraverse();
-     *          2. private void postorderTraverse(BinaryNode<T> node)
-     *          3. public void postorderTraverse_callBinaryNodeMethod()
-     *     . In BinaryNode.java
-     *          4. public void postorderTraverse_binaryNodeMethod() */
-    
     /**
      * calls postorderTraverse(BinaryNode<T> node) prints (using post-order
      * traversal) all nodes in the "whole" tree
      */
     public void postorderTraverse() {
         postorderTraverse(root);
-    }
+    } // end postorderTraverse
     
     /**
      * A Recursive Method in the BinaryTree Class prints (using post-order
      * traversal) all nodes in the subtree rooted at this node.
      */
     private void postorderTraverse(BinaryNode<T> node) {
-        if(node != null)
-        {
+        if(node != null) {
             postorderTraverse(node.getLeftChild());
             postorderTraverse(node.getRightChild());
             System.out.println(node.getData());
-        }
-    }
+        } // end if
+    } // end postorderTraverse
     
     /**
      * The following calls postorderTraverse_binaryNodeMethod(), which is a
@@ -103,14 +147,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
      */
     public void postorderTraverse_callBinaryNodeMethod() {
         root.postorderTraverse_binaryNodeMethod(root);
-    }
-    
-    /* -------------------------------------------------------------------- */
-    /* Task 2: Implement the 2 methods
-     *     . In BinaryTree.java
-     *          1. public int getHeight_callBinaryNodeMethod()
-     *     . In BinaryNode.java
-     *          2. public int getHeight_binaryNodeMethod()*/
+    } // end postorderTraverse_callBinaryNodeMethod
     
     /**
      * calls getHeight(BinaryNode<T> node)
@@ -145,12 +182,6 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
         return root.getHeight_binaryNodeMethod();
     } // end getHeight_callBinaryNodeMethod
     
-    /* -------------------------------------------------------------------- */
-    /* Task 3: Implement the 2 methods
-     *     . In BinaryTree.java
-     *          1. public int getNumberOfNodes()
-     *          2. private int getNumberOfNodes(BinaryNode<T> node)*/
-    
     /**
      * calls getNumberOfNodes(BinaryNode<T> node)
      *
@@ -170,15 +201,13 @@ public class BinaryTree<T> implements BinaryTreeInterface<T> {
         int leftNumber = 0;
         int rightNumber = 0;
 
-        if(node.getLeftChild() != null)
-        {
+        if(node.getLeftChild() != null) {
             leftNumber = getNumberOfNodes(node.getLeftChild());
-        }
+        } // end if
 
-        if(node.getRightChild() != null)
-        {
+        if(node.getRightChild() != null) {
             rightNumber = getNumberOfNodes(node.getRightChild());
-        }
+        } // end if
 
         return 1 + leftNumber + rightNumber;
         
